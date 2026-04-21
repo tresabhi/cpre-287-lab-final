5173194e1468972da8cbe3b69567a049a184cb01
tresabhi <43380238+tresabhi@users.noreply.github.com>
2026-04-21 12:58:26 -0500

    primary to secondary socket

--- heaters_coolers.py
+++ heaters_coolers.py
@@ -6,3 +6,6 @@ cooling_pin = board.A1
 
 heating_pin = digitalio.DigitalInOut(heating_pin)
 cooling_pin = digitalio.DigitalInOut(cooling_pin)
+
+heating_pin.direction = digitalio.Direction.OUTPUT
+cooling_pin.direction = digitalio.Direction.OUTPUT
